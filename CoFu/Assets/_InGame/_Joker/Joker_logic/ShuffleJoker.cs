5d8c49f8a1bf11538690d664c85c45a7b27d7cf2
thehouseofcolors <rengin.celik97@gmail.com>
2025-11-05 10:56:57 +0300

    cleaning

--- CoFu/Assets/_InGame/_Joker/Joker_logic/ShuffleJoker.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-//buraya jokerin lojik kısmı gelecek nasıl hiçbir fikrim yok.
-public class ShuffleJoker : IJoker
-{
-    public bool TryUseJoker()
-    {
-        throw new System.NotImplementedException();
-    }
-}
